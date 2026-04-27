dcd1859731fe198a905aa3384403bc407e76420e
gamerProg <gamer.dan444@gmail.com>
2026-04-27 15:37:58 +0530

    gamerProg added UC10

---
+++ App/src/LengthUnit2.java
@@ -0,0 +1,29 @@
+public enum LengthUnit2 implements IMeasurable {
+
+    FEET(1.0),
+    INCH(1.0 / 12.0),
+    YARD(3.0),
+    CM(0.393701 / 12.0);
+
+    private final double factor;
+
+    LengthUnit(double factor) {
+        this.factor = factor;
+    }
+
+    public double getConversionFactor() {
+        return factor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * factor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / factor;
+    }
+
+    public String getUnitName() {
+        return this.name();
+    }
+}
---
+++ App/src/WeightUnit2.java
@@ -0,0 +1,28 @@
+public enum WeightUnit2 implements IMeasurable {
+
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
+
+    private final double factor;
+
+    WeightUnit(double factor) {
+        this.factor = factor;
+    }
+
+    public double getConversionFactor() {
+        return factor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * factor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / factor;
+    }
+
+    public String getUnitName() {
+        return this.name();
+    }
+}
